e8f145fa9237ab7facb499b3afcde58e5d13837d
sergei.kolbasin <sergei.kolbasin@gmail.com>
2015-05-25 16:38:18 +0300

    added missed unit tests configuration file

---
+++ karma.conf.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Mon May 25 2015 11:14:37 GMT+0300 (FLE Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      './public/plugins/jquery.min.js',
+      './bower_components/angular/angular.js',
+      './node_modules/angular-mocks/angular-mocks.js',
+      './bower_components/ng-file-upload/ng-file-upload.js',
+      './bower_components/canvas-to-blob/js/canvas-to-blob.js',
+      './public/site.js',
+      './test/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      './public/site.js': ['coverage']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'coverage'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers 'PhantomJS', 'Chrome'
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
